f0e7ae8dfec8eab48618b640a385e2effd552082
Daniel Gutierrez Duque <danielgudu22@gmail.com>
2020-01-17 11:55:52 -0500

    set get allCurrencies

--- client/src/components/Currencies.jsx
+++ client/src/components/Currencies.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Mutation } from "react-apollo";
+import React, { useState, useEffect } from "react";
+import { Mutation, Query } from "react-apollo";
 import { gql } from "apollo-boost";
 
 import { Container, Button, Table } from "react-bootstrap";
@@ -8,6 +8,17 @@ import ModalForm from "./FormCurrency/ModalForm";
 const DELETE_MUTATION = gql`
   mutation DeleteMutation($id: Int!) {
     deleteGasto(id: $id)
+  }
+`;
+
+const getCurrencies = gql`
+  {
+    allGastos {
+      id
+      name
+      valor
+      type
+    }
   }
 `;
 
@@ -21,58 +32,58 @@ const Currencies = props => {
     window.location.reload();
   };
 
-  const { loading, allGastos } = props.data;
-
-  if (loading) return <h3>Loading...</h3>;
-
   return (
-    <Container className="mt-5">
-      <ModalForm
-        action="Create"
-        show={show}
-        handleClose={handleClose}
-        refresh={refreshPage}
-      />
-      <Button style={{ float: "left" }} className="mb-3" onClick={handleShow}>
-        Add new currency
-      </Button>
-      <Table striped bordered hover variant="dark">
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Value</th>
-            <th>Type</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {allGastos?.map(gasto => {
-            let id = parseInt(gasto.id)
-            return (
-              <tr key={gasto.id}>
-                <td>{gasto.id}</td>
-                <td>{gasto.name}</td>
-                <td>{gasto.valor}</td>
-                <td>{gasto.type}</td>
-                <td>
-                  <Button variant="primary" className="mr-2">
-                    Update
-                  </Button>
-                  <Mutation mutation={DELETE_MUTATION} variables={{ id }}>
-                    {deleteMutation => (
-                      <Button variant="danger" onClick={deleteMutation}>
-                        Delete
-                      </Button>
-                    )}
-                  </Mutation>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
-    </Container>
+    <Query query={getCurrencies}>
+      {({ data }) => (
+      <Container className="mt-5">
+        <ModalForm
+          action="Create"
+          show={show}
+          handleClose={handleClose}
+          refresh={refreshPage}
+        />
+        <Button style={{ float: "left" }} className="mb-3" onClick={handleShow}>
+          Add new currency
+        </Button>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Value</th>
+              <th>Type</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.allGastos.map(gasto => {
+              let id = parseInt(gasto.id);
+              return (
+                <tr key={gasto.id}>
+                  <td>{gasto.id}</td>
+                  <td>{gasto.name}</td>
+                  <td>{gasto.valor}</td>
+                  <td>{gasto.type}</td>
+                  <td>
+                    <Button variant="primary" className="mr-2">
+                      Update
+                    </Button>
+                    <Mutation mutation={DELETE_MUTATION} variables={{ id }}>
+                      {deleteMutation => (
+                        <Button variant="danger" onClick={deleteMutation}>
+                          Delete
+                        </Button>
+                      )}
+                    </Mutation>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </Container>
+      )}
+    </Query>
   );
 };
 
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import { gql } from 'apollo-boost';
-import { graphql } from 'react-apollo';
+import { graphql, Query } from 'react-apollo';
 
 import { Container } from 'react-bootstrap';
 import Currencies from './Currencies';
 
-const Main = props => {
+const Main = () => {
     return (  
-        <Container className="justify-content-center text-center" style={{margin: "auto"}}>
-            <h1>Currency Manager</h1>
-            <Currencies data={props.data} />
-        </Container>
+      <Container className="justify-content-center text-center" style={{margin: "auto"}}>
+        <h1>Currency Manager</h1>
+        <Currencies />
+      </Container>
     );
 }
-
-const getCurrencies = gql`
-  {
-    allGastos {
-        id
-        name
-        valor
-        type
-    }
-  }   
-`;
  
-export default graphql(getCurrencies)(Main);
+export default Main;

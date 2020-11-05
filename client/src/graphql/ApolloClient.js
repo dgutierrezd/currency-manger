6edfd865db7437916fc1a73a7c2aa36d677faa90
Daniel Gutierrez <danielgudu22@gmail.com>
2020-11-05 15:21:38 -0500

    set port in react

--- client/src/graphql/ApolloClient.js
+++ client/src/graphql/ApolloClient.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-    uri: "http://localhost:4023/graphql"
+    uri: `http://localhost:${process.env.PORT}/graphql`
 });
 
 export default client;

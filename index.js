c5820fef6da5fe0dd16e2f4db51fb21a43395cfa
Daniel Gutierrez <danielgudu22@gmail.com>
2020-11-05 13:48:10 -0500

    build react

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ if(process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 8000
 
 app.listen(port, () => { 
   console.log("App is running on port " + port); 

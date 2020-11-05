f6a54d452a1fd1b3cdf08d0e58b492cd5e0951eb
Daniel Gutierrez <danielgudu22@gmail.com>
2020-11-05 12:45:30 -0500

    change port to deploy

--- index.js
+++ index.js
@@ -19,15 +19,13 @@ server.applyMiddleware({ app });
 if(process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  });
 }
 
-app.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+const port = process.env.PORT || 4000
+
+app.listen(port, () => { 
+  console.log("App is running on port " + port); 
+}); 
 
 /* app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)

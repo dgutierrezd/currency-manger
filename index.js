f4609e2b4118a1dae78aacec75bdb9f576a46bd0
Daniel Gutierrez <danielgudu22@gmail.com>
2020-11-05 13:12:57 -0500

    change index server

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ server.applyMiddleware({ app });
 if(process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
 }
 
 const port = process.env.PORT || 4000

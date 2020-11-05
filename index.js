b34c62b3e643e5f29b55fb2a0987126f8a277c08
Daniel Gutierrez <danielgudu22@gmail.com>
2020-11-05 14:32:42 -0500

    change port

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
-const path = require('path');
-const cors = require('cors');
+const path = require("path");
+const cors = require("cors");
 
-const typeDefs = require('./schema') ;
-const resolvers = require('./resolvers') ;
-const models = require('./models') ;
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const models = require("./models");
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: { models }
+  typeDefs,
+  resolvers,
+  context: { models },
 });
 
 const app = express();
 app.use(cors());
 server.applyMiddleware({ app });
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || '8080';
 
-app.listen(port, () => { 
-  console.log("App is running on port " + port); 
-}); 
+app.listen(port, () => {
+  console.log("App is running on port " + port);
+});
 
 /* app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
